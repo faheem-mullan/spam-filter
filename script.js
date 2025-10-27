9fe507fc347726dea3378b695cf9f80095f01764
Faheem <faheemxxx456@gmail.com>
2025-10-27 21:49:51 +0530

    made a array method to filter out spam using some()

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const denyList=[helpRegex];
 checkMessageButton.addEventListener("click", () => {
   if (messageInput.value === "") {
     alert("Please enter a message.");
-    return;
+  return;
   }
 
   result.textContent = isSpam(messageInput.value)
