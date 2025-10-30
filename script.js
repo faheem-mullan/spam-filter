539ae7e57ccf5a2788ebf39b8b36c32480d5e74c
Faheem <faheemxxx456@gmail.com>
2025-10-30 20:28:23 +0530

    initial commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
 const helpRegex = /please help|assist me/i;
-const dollarRegex=/dollars/i;
+const dollarRegex=/[0-9]+ (hundred|millions|thousand)?dollars/i;
 
 const isSpam = (msg) => denyList.some(regex=>regex.test(msg));
 const denyList=[helpRegex];
