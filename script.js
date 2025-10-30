65ab39c7ddbbfacfa796b65a25eb02ca7380cce7
Faheem <faheemxxx456@gmail.com>
2025-10-30 21:07:07 +0530

    free-money filter

--- script.js
+++ script.js
@@ -3,15 +3,17 @@ const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
 const helpRegex = /please help|assist me/i;
-const dollarRegex=/[0-9]+ (hundred|millions|thousand)?dollars/i;
+const dollarRegex = /[0-9]+\s*(?:hundred|thousand|million|billion)?\s+dollars/i;
+const freeRegex = /\sfr[e3][e3] m[o0]n[e3]y\s/i;
 
-const isSpam = (msg) => denyList.some(regex=>regex.test(msg));
-const denyList=[helpRegex];
+const denyList = [helpRegex, dollarRegex, freeRegex];
+
+const isSpam = (msg) => denyList.some((regex) => regex.test(msg));
 
 checkMessageButton.addEventListener("click", () => {
   if (messageInput.value === "") {
     alert("Please enter a message.");
-  return;
+    return;
   }
 
   result.textContent = isSpam(messageInput.value)
